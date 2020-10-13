bb503f8e4d1278bf8b718c0c66a712e34e20ce27
beatricemagana <beemag06@gmail.com>
2020-10-13 10:25:24 -0700

    add click event

--- script.js
+++ script.js
@@ -7,5 +7,7 @@ const movieSelect = document.getElementById('movie');
 const ticketPrice = movieSelect.value;
 
 container.addEventListener('click', (e) => {
-    console.log(e.target);
+    if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+      e.target.classList.toggle('selected');
+    }
 });
